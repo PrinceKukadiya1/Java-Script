fe78b8f4a0e9631d0e7f1f48d1201bf74d9b1584
prince kukadiya <princekukadiya2304@gmail.com>
2025-01-30 13:46:56 +0000

    variable

---
+++ basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hello@gmail.com"
+var accounntPassword = "12345"
+accountCity = "Jaipur"
+let accountStates;
+
+//accountId = 2
+
+console.log(accountId);
+
+/*
+var is not use now day.
+because of  issue in block scope and functional scope 
+*/
+
+console.table([accountId,accountEmail,accounntPassword,accountCity,accountStates])
