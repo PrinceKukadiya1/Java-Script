c7cd6169e799705c7aee7e96142535ea8705aba4
prince kukadiya <princekukadiya2304@gmail.com>
2025-01-30 14:30:10 +0000

    comparision

---
+++ basic/04_comparision.js
@@ -0,0 +1,6 @@
+console.log(2 > 0);
+console.log(2 < 0);
+console.log(2 >= 0);
+console.log(2 == 0);
+console.log(2 != 0);
+
---
+++ practice/variable.js
@@ -0,0 +1,7 @@
+let Myname = "prince"
+let surname = "Kukadiya"
+let age = 20
+let ContactNo = 1234567891
+let EmailId = "Prince12@gmail.com" 
+
+console.table([Myname,surname,age,ContactNo,EmailId])
