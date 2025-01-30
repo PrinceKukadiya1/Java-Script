67a78aebf160816a61b16599d66d091f3f626e4e
prince kukadiya <princekukadiya2304@gmail.com>
2025-01-30 14:04:27 +0000

    conversion

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "35"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// conversion
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefine => NaN
+// boolean => 0/1
