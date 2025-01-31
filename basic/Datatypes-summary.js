1a9c12b17ab5a2abb42ee7779bc8266e9418ee9e
prince kukadiya <princekukadiya2304@gmail.com>
2025-01-31 13:44:46 +0530

    Information about datatypes

---
+++ basic/Datatypes-summary.js
@@ -0,0 +1,34 @@
+// JavaScript is a dynamically typed language.
+
+/*
+Primitive datatypes
+ 7 Types 
+
+1)string
+2)Number
+3)Boolean
+4)Null
+5)Undefine
+6)Symbols
+7)BigInt
+*/
+
+/* 
+Non-Premitive(Reference)
+
+1)Array
+2)Objects
+3)Functions
+*/
+
+// Example
+const heros = ["Spiderman","Betman","shaktiman"]
+
+let myobj = {
+    No : 22,
+    name : "prince"
+}
+
+const myfunction = function(){
+    console.log("Hello")
+}
