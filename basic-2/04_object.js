9a8f511b2152ceb3966cf954a64194f6bdf0aad2
prince kukadiya <princekukadiya2304@gmail.com>
2025-02-02 12:18:01 +0530

    object part2

---
+++ basic-2/04_object.js
@@ -0,0 +1,14 @@
+// const tinderuser = new Object() ---> singleton object
+
+const tinderuser = {}  //non singleton object
+
+console.log(tinderuser)
+
+const obj1 = {1: "a" , 2: "b"}
+const obj2 = {3: "a" , 4: "b"}
+const obj3 = {5: "a" , 6: "b"}
+
+// const obj4 = Object.assign({},obj1,obj2,obj3)
+
+const obj4 = {...obj1,...obj2,...obj3}
+console.log(obj4)
