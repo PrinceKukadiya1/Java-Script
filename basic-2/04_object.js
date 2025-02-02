f4d2d7f412dc32bde1658c339ce050b791d74296
prince kukadiya <princekukadiya2304@gmail.com>
2025-02-02 18:17:43 +0530

    change

--- basic-2/04_object.js
+++ basic-2/04_object.js
@@ -12,3 +12,14 @@ const obj3 = {5: "a" , 6: "b"}
 
 const obj4 = {...obj1,...obj2,...obj3}
 console.log(obj4)
+
+
+const course = {
+    coursename :  "js in hindi",
+    course_price: "999",
+    course_instructor:"hitesh"
+}
+
+const {course_instructor} = course
+
+console.log(course_instructor)
