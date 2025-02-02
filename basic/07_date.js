236aac25fe9061a1d52b2251bdc4f81aa0871e42
prince kukadiya <princekukadiya2304@gmail.com>
2025-02-02 10:06:53 +0530

    date and time

---
+++ basic/07_date.js
@@ -0,0 +1,8 @@
+// dates
+
+const mydate = new Date()
+console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toJSON())
+console.log(mydate.toLocaleDateString())
+console.log(mydate.toTimeString())
