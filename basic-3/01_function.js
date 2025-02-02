9fe7bef67e64d46051ccb8686267f9081fef7a22
prince kukadiya <princekukadiya2304@gmail.com>
2025-02-02 18:46:21 +0530

    changes

---
+++ basic-3/01_function.js
@@ -0,0 +1,24 @@
+
+
+function myname(){
+    console.log("p");
+    console.log("r");
+    console.log("i");
+    console.log("n");
+    console.log("c");
+    console.log("e");
+    
+}
+
+// function price(function name)[ () scope of function] [{}--> defination of function]
+
+// myname --> it is a referebce of the function
+
+// when we create a function and take a value is called parameters
+// when we call a function and take a value is called argument
+
+function addtwonumber(num1,num2){
+    console.log(num1+num2)
+}
+
+addtwonumber(5,6)
