8614fbef654bc087bb6c0c007d3046fcb0c19105
prince kukadiya <princekukadiya2304@gmail.com>
2025-02-02 10:31:29 +0530

    array part-1

---
+++ basic-2/01_array.js
@@ -0,0 +1,12 @@
+const myArr = [0,1,2,3,4,5]
+console.log(myArr[0]);
+
+const heros = new Array("shaktiman" , "betman")
+
+heros.push("krish","monalisa")
+heros.pop()
+console.log(heros);
+
+const newone = heros.join()
+
+console.log(newone)
