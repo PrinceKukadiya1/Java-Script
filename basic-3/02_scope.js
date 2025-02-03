b354e959b5546bc76925bc5ee59fe47c61401e28
prince kukadiya <princekukadiya2304@gmail.com>
2025-02-03 09:09:49 +0530

    scope

---
+++ basic-3/02_scope.js
@@ -0,0 +1,26 @@
+var a = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+console.log(a);
+// console.log(b);
+console.log(c);
+
+// different types of describing function
+function addone(num){
+    return num + 1
+}
+
+// console.log(addone(5))
+
+
+// sometime it's call expression
+const addtwo = function(num){
+    return num + 2
+}
+
+// console.log(addtwo(5))
