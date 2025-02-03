6c8388725ba3ee4fb3e72ba9c25b4859215426ac
prince kukadiya <princekukadiya2304@gmail.com>
2025-02-03 17:49:46 +0530

    control flow

---
+++ basic-4/one.js
@@ -0,0 +1,4 @@
+//if(condition ){ ........ }
+
+let balance =499
+if(balance < 500) console.log("test1");
---
+++ basic-4/truthy.js
@@ -0,0 +1,16 @@
+// falsy value
+// false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN
+
+// truthy value
+// "0" ,"false" , " " , [] , {} , function(){}
+
+
+const useremail = {}
+
+if (useremail) {
+    console.log("user login"); 
+}
+else{
+    console.log("user not login");
+    
+}
